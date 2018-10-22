ef84855e8f815ae14e47768d953b3986c8463d51
bob620 <bruder.kraft225@gmail.com>
2018-10-22 02:44:31 -0500

    fix(timestampToUlid) timestamp being a string, needed int

--- util/uuidmodify.js
+++ util/uuidmodify.js
@@ -6,6 +6,7 @@ module.exports = {
 		return ulid.substr(10, 9) + ulid.substr(5, 5) + ulid.substr(0, 5) + ulid.substr(19);
 	},
 	timestampToUlid: timestamp => {
+		timestamp = Number.parseInt(timestamp);
 		const hiAndMid = (((timestamp + 12219292800000) / 0x100000000 * 10000) & 0xfffffff).toString(16);
 		return `1${hiAndMid.substr(0, 3)}-${hiAndMid.substr(3)}-${((((timestamp + 12219292800000) & 0xfffffff) * 10000) % 0x100000000).toString(16)}`;
 	}
