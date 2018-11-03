a3e5c0adcfe85991b505a8545834ef7cca047d2f
bob620 <bruder.kraft225@gmail.com>
2018-11-03 17:14:53 -0500

    feat(index) added commented out exit handler

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+/*
+#!/usr/bin/env node
+
+process.stdin.resume();
+
+let hasClosed = false;
+function exitHandler() {
+	if (!hasClosed) {
+		hasClosed = true;
+		console.log("Shutting down...");
+
+		process.exit();
+	}
+}
+
+// do something when app is closing
+process.on('exit', exitHandler.bind(null));
+
+// catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null));
+
+// catches "kill pid"
+process.on('SIGUSR1', exitHandler.bind(null));
+process.on('SIGUSR2', exitHandler.bind(null));
+process.on('SIGTERM', exitHandler.bind(null));
+
+// catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null));
+*/
 const Random = require('random-js');
 const random = new Random(Random.engines.mt19937().autoSeed());
 const WS = require('ws');
